b97339888baaafb20717fa3ffc97540ab0999886
Xuejia KE <55718993+thebestecho@users.noreply.github.com>
2020-08-16 22:41:10 +0100

    Update transform_code_format.R

--- transform_code_format.R
+++ transform_code_format.R
@@ -1,24 +1,19 @@
-# Create a new directory in the current working directory
-dir.create(here::here("simulated_data"))
-ORF <- paste0("G",1:20)
-set.seed(5) # For reproducing the same data
-Total <- floor (rlnorm (20, log (8000), log (1.5)))
-pSup <- runif (20, min=0, max=1) # pSup for condition 30C
-# scaling factor for Sup in condition 30C
-scaling_factor1S <- runif (1, min=0, max=1)
-# scaling factor for Pellet in condition 30C
-scaling_factor1P <- runif (1, min=0, max=1) 
-Tot1 <- rpois (20, lambda = 1000) # N total for condition 30C
-Sup1 <- rpois (20, lambda = Total*scaling_factor1S*pSup) # N sup
-Pellet1 <- rpois (20, lambda = Total*scaling_factor1P*(1-pSup)) # N pellet
-# Repeat for condition 42C
-set.seed(10)
-scaling_factor2S <- runif (1, min=0, max=1)
-scaling_factor2P <- runif (1, min=0, max=1)
-Tot2 <- rpois (20, lambda = 1000) # N total for condition 42C
-Sup2 <- rpois (20, lambda = Total*scaling_factor2S*pSup) # N sup
-Pellet2 <- rpois (20, lambda = Total*scaling_factor2P*(1-pSup)) # N pellet
-# Write data into sample files (for example)
-sample1 <- data.frame(ORF, Tot1) 
-write_tsv (sample1,
-          here::here("simulated_data","sample1.txt"),col_names = FALSE)
+# Read the raw data
+raw_data = readr::read_tsv("./GSE141029_counts_matrix.txt")
+# Replace the column names and covert it into longer format
+wide_published_data <- tidyr::pivot_longer ( raw_data,
+cols = "IDR_30C_1_pellet_262":"WT_42C_2_total_219",
+names_to = c (“Condition","Replicate","Fraction","File"),
+names_pattern = "(. * _.*)_(.*)_(.*)_(.*)",
+values_to = "Count") %>% 
+  dplyr::select(-File) %>% 
+  tidyr::pivot_wider(names_from = "Fraction",values_from = "Count")
+
+colnames(wide_published_data) [c (1,4:6)] <- c("ORF","Pellet","Sup","Tot")
+# Filter data
+ORFselect = readr::read_tsv("./original_Scer_ORF_length.txt")
+wide_published_filtered <- wide_published_data %>%
+  tidyr::unite(Condition,Condition,Replicate,sep = "_") %>%
+  dplyr::filter(ORF %in% ORFselect$ORF, Pellet > 0, Sup > 0, Tot > 0)
+# Get pSup values
+published_data_pSup <- each_mRNA_pSup (wide_published_filtered)
