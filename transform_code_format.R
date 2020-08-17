d5ad53d7f6bdd1e06731a7d866e299002b34c3d9
Xuejia KE <55718993+thebestecho@users.noreply.github.com>
2020-08-17 01:29:56 +0100

    Update transform_code_format.R

--- transform_code_format.R
+++ transform_code_format.R
@@ -1,22 +1,16 @@
-# Read the raw data
-raw_data = readr::read_tsv("./GSE141029_counts_matrix.txt")
-# Covert it into longer format
-wide_published_data <- tidyr::pivot_longer ( raw_data,
-cols = "IDR_30C_1_pellet_262":"WT_42C_2_total_219",
-names_to = c (“Condition","Replicate","Fraction","File"), 
-names_pattern = "(. * _.*)_(.*)_(.*)_(.*)",
-values_to = "Count") %>% 
-  dplyr::select(-File) %>% 
-  tidyr::pivot_wider(names_from = "Fraction",values_from = "Count")
+# Filter for only selected ORFs, and calculate TPMs
+TSP_Count_TPM <- TSP_Count %>%
+    inner_join(ORFselect) %>%
+    group_by(Sample,Condition,Fraction) %>%
+    mutate(Density=Count/Length, 
+           TPM=Density/sum(Density)*1e6  ) %>%
+    select(Sample,Condition,Fraction,ORF,Count,TPM)
 
-# Replace the column names
-colnames(wide_published_data) [c (1,4:6)] <- c("ORF","Pellet","Sup","Tot")
-
-# Filter data
-ORFselect = readr::read_tsv("./original_Scer_ORF_length.txt")
-wide_published_filtered <- wide_published_data %>%
-  tidyr::unite(Condition,Condition,Replicate,sep = "_") %>%
-  dplyr::filter(ORF %in% ORFselect$ORF, Pellet > 0, Sup > 0, Tot > 0)
-# Get pSup values
-published_data_pSup <- each_mRNA_pSup (wide_published_filtered)
+# Transform the data frame into the wider one
+TSP_TPM_wide <- 
+    TSP_Count_TPM %>%
+    ungroup() %>%
+    select(Condition,Fraction,ORF,TPM) %>%
+    unite(Var,Condition,Fraction,sep="_") %>%
+    spread(Var,TPM)
 
