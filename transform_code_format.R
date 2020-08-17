208c60aac45f06aa8734c2bf282c648dd3d4413f
Xuejia KE <55718993+thebestecho@users.noreply.github.com>
2020-08-17 02:08:29 +0100

    Update transform_code_format.R

--- transform_code_format.R
+++ transform_code_format.R
@@ -1,16 +1,77 @@
-# Filter for only selected ORFs, and calculate TPMs
-TSP_Count_TPM <- TSP_Count %>%
-    inner_join(ORFselect) %>%
-    group_by(Sample,Condition,Fraction) %>%
-    mutate(Density=Count/Length, 
-           TPM=Density/sum(Density)*1e6  ) %>%
-    select(Sample,Condition,Fraction,ORF,Count,TPM)
+mdat_mixingTSP <- function(ctdata) {
+    ## make data for stan fit
+    head(ctdata)
+    list(NRNA=nrow(ctdata),
+         tot_obs=as.integer(round(ctdata$Tot)),
+         sup_obs=as.integer(round(ctdata$Sup)),
+         p100_obs=as.integer(round(ctdata$P100))
+         )
+    
+}
 
-# Transform the data frame into the wider one
-TSP_TPM_wide <- 
-    TSP_Count_TPM %>%
-    ungroup() %>%
-    select(Condition,Fraction,ORF,TPM) %>%
-    unite(Var,Condition,Fraction,sep="_") %>%
-    spread(Var,TPM)
+make_mixingTSP <- function(ctdata) {
+    stan_dat <- mdat_mixingTSP(ctdata)
+    stan(model_code='// -*- mode: C -*-
+data {
+  // Number of RNAs
+  int<lower=1> NRNA;     
+  
+  // Note: These are all integers
+  // columns t, s, p
+  int<lower=0> tot_obs[NRNA];
+  int<lower=0> sup_obs[NRNA];
+  int<lower=0> p100_obs[NRNA];
+}
+parameters {
+  // Unnormalized mixing proportions
+  // real<lower=0> mixing_t;
+  real<lower=0> mixing_sup;
+  real<lower=0> mixing_p100;
+  
+  // dispersion parameter for counts
+  real phi;
+}
+model{
+  // mixing ratios
+  mixing_sup ~ gamma(1,1);
+  mixing_p100 ~ gamma(1,1);
+  // Cauchy prior for negbin dispersion parameter
+  phi ~ cauchy(0,3);
+  
+  for(idx in 1:NRNA){ 
+    // count distn negative binomial with specified means
+    // Total
+    tot_obs[idx] ~ neg_binomial_2(mixing_sup * sup_obs[idx] + mixing_p100 * p100_obs[idx], phi);
+  }
 
+}
+generated quantities{
+  // print("Mixing pars (sup,p100) = (", mixing_sup,",",mixing_p100,")");
+  // print("dispersion phi = ", phi);
+  // print("------------------");
+}
+',
+data=stan_dat,chains = 1,iter = 10)
+}
+
+fit_mixingTSP <- function(ctdata,stan_mixing=NULL,...) {
+    stan_dat <- mdat_mixingTSP(ctdata)
+    if (is.null(stan_mixing)) {
+        stan_mixing <- make_mixingTSP(ctdata)
+    }
+    stan_mixing_fit <- stan(fit=stan_mixing,data=stan_dat,chains = 4,...)
+    return(stan_mixing_fit)
+}
+
+getmixingratiosTSP <- function(ctdata,iter=1000,
+                                control=list(adapt_delta=0.85),...) {
+    # head(ctdata) %>% print()
+    stansummary <- fit_mixingTSP(ctdata=ctdata,iter=iter,control=control,...) %>%
+        summary()
+    # return medians
+    data.frame(
+        mixing.Sup=stansummary$summary["mixing_sup","50%"],
+        mixing.P100=stansummary$summary["mixing_p100","50%"],
+        lp.n_eff  =stansummary$summary["lp__","n_eff"],
+        lp.Rhat   =stansummary$summary["lp__","Rhat"])
+}
