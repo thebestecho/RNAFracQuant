11f93eacb67439ca3eaa9f1a3e28e32b850bb5ab
Xuejia KE <55718993+thebestecho@users.noreply.github.com>
2020-08-17 03:36:17 +0100

    Update transform_code_format.R

--- transform_code_format.R
+++ transform_code_format.R
@@ -1,12 +1,4 @@
-getmixingratiosTSP <- function(ctdata,iter=1000,
-                                control=list(adapt_delta=0.85),...) {
-    # head(ctdata) %>% print()
-    stansummary <- fit_mixingTSP(ctdata=ctdata,iter=iter,control=control,...) %>%
-        summary()
-    # return medians
-    data.frame(
-        mixing.Sup=stansummary$summary["mixing_sup","50%"],
-        mixing.P100=stansummary$summary["mixing_p100","50%"],
-        lp.n_eff  =stansummary$summary["lp__","n_eff"],
-        lp.Rhat   =stansummary$summary["lp__","Rhat"])
-}
+# run stan mixing ratio inference, reusing same model
+mixing_ratios_TSP <- TSP_Count_FracBySample %>%
+    ddply(~Condition,getmixingratiosTSP,
+          stan_mixing=stan_mixingTSP_wd5,iter=1000,seed=myseed)
